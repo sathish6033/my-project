ce53b16964e06f2fc9fb3ea3a727dfbdc60d4330
sathi <sathi@192.168.0.15>
2019-09-05 11:30:24 -0400

    Github project

---
+++ Github/src/github/Github_class.java
@@ -0,0 +1,18 @@
+package github;
+
+public class Github_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =20;
+		int b = 30;
+		int c = a+b;
+		System.out.println(c);
+	}
+	
+	public void add(){
+		
+	}
+
+}
